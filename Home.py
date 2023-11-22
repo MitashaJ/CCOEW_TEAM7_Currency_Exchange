aaa330833105cc4a2afbf49ed102c7d49048a9bb
Tejaswini2004313 <tejaswinipatkar55@gmail.com>
2023-11-23 02:55:14 +0530

    Updated home

--- Home.py
+++ Home.py
@@ -1,15 +1,18 @@
-import streamlit as st
-import requests 
+import streamlit as st  #pip install streamlit
+import requests  #pip install requests
 import json
-from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie  #pip install streamlit_lottie
 
+#set page title
 st.set_page_config(
     page_title="Currency Converter"
 )
 
+#set title and sidebar
 st.title("Currency Conversion")
 st.sidebar.success("Select a page above")
 
+#animation
 def load_lottiefile(filepath: str):
     with open(filepath,"r") as f:
         return json.load(f)
@@ -17,8 +20,8 @@ def load_lottiefile(filepath: str):
 lottie_currency = load_lottiefile("E:\\NT_hackathon\\CCOEW_TEAM7_Currency_Exchange\\animation.json")
 st_lottie(lottie_currency, quality='high')
 
+#inputs for currencies
 col1, col2 = st.columns(2)
-
 with col1:
     global curr1 
     curr1 = st.selectbox('Currency 1', ['AUD','BHD','BWP','BRL','BND','CAD','CLP','CNY','COP','CZK','DKK','EUR','HUF','ISK','INR','IDR','IRR','ILS','JPY','KZT','KRW','KWD','LYD','MYR','MUR','MXN',
@@ -29,6 +32,7 @@ with col2:
     curr2 = st.selectbox('Currency 2', ['AUD','BHD','BWP','BRL','BND','CAD','CLP','CNY','COP','CZK','DKK','EUR','HUF','ISK','INR','IDR','IRR','ILS','JPY','KZT','KRW','KWD','LYD','MYR','MUR','MXN',
                                         'NPR','NZD','NOK','OMR','PKR','PEN','PHP','PLN','QAR','RUB','SAR','SGD','ZAR','LKR','SEK','CHF','THB','TTD','TND','AED','GBP','USD','UYU','VES'])
 
+#api url
 url = f"https://v6.exchangerate-api.com/v6/5063b86e4e65a0219579c7fc/pair/{curr1}/{curr2}/1"
 
 re = requests.get(url)
@@ -41,6 +45,7 @@ with col1:
 with col2:
     converted = amount*data
     st.text('Converted amount')
-    st.success(converted)
+    st.success(converted)  #displaying converted amount
     
+#styling
 st.markdown('<style> body{text-align:center;}</style>',unsafe_allow_html=True)
