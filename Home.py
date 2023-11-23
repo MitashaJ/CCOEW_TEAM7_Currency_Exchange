851edbce85fd1d0255848d188b999f75ff8ed136
Tejaswini2004313 <tejaswinipatkar55@gmail.com>
2023-11-23 10:22:48 +0530

    Home updated

--- Home.py
+++ Home.py
@@ -36,32 +36,41 @@ def main():
     lottie_currency = load_lottiefile("E:\\NT_hackathon\\CCOEW_TEAM7_Currency_Exchange\\animation.json")
     st_lottie(lottie_currency, quality='high')
 
-#inputs for currencies
-col1, col2 = st.columns(2)
-with col1:
-    global curr1 
-    curr1 = st.selectbox('Currency 1', ['AUD','BHD','BWP','BRL','BND','CAD','CLP','CNY','COP','CZK','DKK','EUR','HUF','ISK','INR','IDR','IRR','ILS','JPY','KZT','KRW','KWD','LYD','MYR','MUR','MXN',
-                                        'NPR','NZD','NOK','OMR','PKR','PEN','PHP','PLN','QAR','RUB','SAR','SGD','ZAR','LKR','SEK','CHF','THB','TTD','TND','AED','GBP','USD','UYU','VES'])
+    # Create a form for the currency converter
+    with st.form("currency_converter_form"):
+        # Create two columns for currency selection
+        col1, col2 = st.columns(2)
+        
+        # Currency 1 selection dropdown
+        with col1:
+            curr1 = st.selectbox('Currency 1', ['AUD', 'BHD', 'BWP', 'BRL', 'BND', 'CAD', 'CLP', 'CNY', 'COP', 'CZK', 'DKK', 'EUR', 'HUF', 'ISK', 'INR', 'IDR', 'IRR', 'ILS', 'JPY', 'KZT', 'KRW', 'KWD', 'LYD', 'MYR', 'MUR', 'MXN',
+                                                'NPR', 'NZD', 'NOK', 'OMR', 'PKR', 'PEN', 'PHP', 'PLN', 'QAR', 'RUB', 'SAR', 'SGD', 'ZAR', 'LKR', 'SEK', 'CHF', 'THB', 'TTD', 'TND', 'AED', 'GBP', 'USD', 'UYU', 'VES'])
+        
+        # Currency 2 selection dropdown
+        with col2:
+            curr2 = st.selectbox('Currency 2', ['AUD', 'BHD', 'BWP', 'BRL', 'BND', 'CAD', 'CLP', 'CNY', 'COP', 'CZK', 'DKK', 'EUR', 'HUF', 'ISK', 'INR', 'IDR', 'IRR', 'ILS', 'JPY', 'KZT', 'KRW', 'KWD', 'LYD', 'MYR', 'MUR', 'MXN',
+                                                'NPR', 'NZD', 'NOK', 'OMR', 'PKR', 'PEN', 'PHP', 'PLN', 'QAR', 'RUB', 'SAR', 'SGD', 'ZAR', 'LKR', 'SEK', 'CHF', 'THB', 'TTD', 'TND', 'AED', 'GBP', 'USD', 'UYU', 'VES'])
+        
+        # Input field for the amount
+        amount = st.number_input('Enter amount')
 
-with col2:
-    global curr2
-    curr2 = st.selectbox('Currency 2', ['AUD','BHD','BWP','BRL','BND','CAD','CLP','CNY','COP','CZK','DKK','EUR','HUF','ISK','INR','IDR','IRR','ILS','JPY','KZT','KRW','KWD','LYD','MYR','MUR','MXN',
-                                        'NPR','NZD','NOK','OMR','PKR','PEN','PHP','PLN','QAR','RUB','SAR','SGD','ZAR','LKR','SEK','CHF','THB','TTD','TND','AED','GBP','USD','UYU','VES'])
+        # Center-align the button using HTML and inline CSS
+        st.write("<div style='text-align: center;'>", unsafe_allow_html=True)
+        
+        # Submit button to perform the conversion
+        if st.form_submit_button("Convert"):
+            conversion_rate = get_conversion_rate(curr1, curr2)
 
-#api url
-url = f"https://v6.exchangerate-api.com/v6/5063b86e4e65a0219579c7fc/pair/{curr1}/{curr2}/1"
+            # Check if conversion rate is obtained successfully
+            if conversion_rate is not None:
+                converted_amount = amount * conversion_rate
+                st.success(f"Converted amount: {converted_amount:.2f} {curr2}")
+        
+        st.write("</div>", unsafe_allow_html=True)
 
-re = requests.get(url)
-data = re.json()['conversion_rate']
+        #styling
+        st.markdown('<style> body{text-align:center;}</style>',unsafe_allow_html=True)
 
-col1, col2 = st.columns(2)
-with col1:
-    global amount
-    amount = st.number_input(curr1)
-with col2:
-    converted = amount*data
-    st.text('Converted amount')
-    st.success(converted)  #displaying converted amount
-    
-#styling
-st.markdown('<style> body{text-align:center;}</style>',unsafe_allow_html=True)
+# Run the main function if the script is executed
+if __name__ == "__main__":
+    main()
